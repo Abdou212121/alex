fc877383080b05f0a4bc11b4a813860ba6ede138
tabbi abderrahmane <abdoutabbi2001@gmail.com>
2021-08-05 21:47:04 +0100

    add hello world

---
+++ criptabc.sh
@@ -0,0 +1,11 @@
+
+
+
+
+echo "# alex" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Abdou212121/alex.git
+git push -u origin main
